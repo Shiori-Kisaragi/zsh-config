46c48844e75f5b358b17be7bbaf24b89141c91bd
Shiori-Kisaragi <144647459+Shiori-Kisaragi@users.noreply.github.com>
2023-10-22 10:59:44 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+ZSH="$HOME/.oh-my-zsh"
+ZSH_CUSTOM="$ZSH/custom"
+if [ ! -d "$ZSH" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+rm "$HOME/.zshrc"
+curl https://raw.githubusercontent.com/Shiori-Kisaragi/zsh-config/master/.zshrc --output "$HOME/.zshrc"
+curl https://raw.githubusercontent.com/Shiori-Kisaragi/zsh-config/master/.p10k.zsh --output "$HOME/.p10k.zsh"
